74598a8f30e9b36d48b1ac1dd6deff263f6506fd
deepak <18l11a0518@gmail.com>
2023-06-07 18:35:09 +0530

    parameters fetched from parameters store

---
+++ run.sh
@@ -0,0 +1,7 @@
+# give input in the format    VARIABLE:PARAMETER, VARIABLE:PARAMETER
+for var in $(echo $1 | sed -e 's/,/' '/g'); do
+    var_name=$(echo $var | awk -F : '{print $1}')
+    parameter=$(echo $var | awk -F : '{print $2}')
+    value=$(aws ssm get-parameter --name $parameter --with-decryption | jq '.Parameter.Value' | xargs)
+    echo ${var_name}=${value}
+done
